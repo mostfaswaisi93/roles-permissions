80fbd5950d5ed75aaea4586604b385e98d31d524
mostfaswaisi93 <mostfaswaisi@gmail.com>
2020-05-08 15:06:10 +0300

    Give, Revoke, Sync, Checking Permissions

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+
+    // 1 - Role
     // return view('welcome');
 
     // auth()->user()->assignRole('admin', 'user');
 
-    auth()->user()->syncRoles(['user']);
+    // auth()->user()->syncRoles(['user']);
 
     // auth()->user()->assignRole('admin');
     // auth()->user()->removeRole('admin');
@@ -15,6 +17,16 @@ Route::get('/', function () {
     // if(auth()->user()->hasRole('admin')){
     //     return "OK";
     // }
+
+
+    // 2- Permission
+    $user = auth()->user();
+
+    // $user->givePermissionTo('add post');
+    // $user->revokePermissionTo('add post');
+    // $user->syncPermissions(['add post', 'delete post']);
+    dd($user->hasAnyPermission(['add post', 'edit post']));
+    // dd($user->hasPermissionTo('add post'));
 
 });
 
